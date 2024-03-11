04497c3672b171c2ff57739485d03beb703f6872
Vlad Zusko <vladzvu11@gmail.com>
2024-03-11 22:01:55 +0200

    fix(sockets/domain): fix connection socket type

--- packages/connections/sockets/domain/SocketConnection.ts
+++ packages/connections/sockets/domain/SocketConnection.ts
@@ -1,8 +1,7 @@
-import type { Server } from 'socket.io';
-import type { Socket } from 'socket.io-client';
-
 import type { Connection } from '@/connections/domain';
 
+import type Socket from './Socket';
+
 export default interface SocketConnection extends Connection {
-  socket: Server | Socket;
+  socket: Socket;
 }
