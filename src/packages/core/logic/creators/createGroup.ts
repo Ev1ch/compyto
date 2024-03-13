d6967dd26e0e84b500dedd0aa4de595a98114a3e
Vlad Zusko <vladzvu11@gmail.com>
2024-03-14 01:13:48 +0200

    fix(core/logic): fix group creator

--- src/packages/core/logic/creators/createGroup.ts
+++ src/packages/core/logic/creators/createGroup.ts
@@ -2,21 +2,26 @@ import type { Group, Process } from '../../domain';
 
 export default function createGroup(): Group {
   const processes: Process[] = [];
+  const add = processes.push.bind(processes);
+
+  function remove(process: Process) {
+    const index = processes.indexOf(process);
+
+    if (index !== -1) {
+      processes.splice(index, 1);
+    }
+  }
+
+  function* iterator() {
+    for (const process of processes) {
+      yield process;
+    }
+  }
 
   return {
     processes,
-    add: processes.push.bind(processes),
-    remove(process) {
-      const index = processes.indexOf(process);
-
-      if (index !== -1) {
-        processes.splice(index, 1);
-      }
-    },
-    [Symbol.iterator]: function* () {
-      for (const process of processes) {
-        yield process;
-      }
-    },
+    add,
+    remove,
+    [Symbol.iterator]: iterator,
   };
 }
