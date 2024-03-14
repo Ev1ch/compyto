6cb52dbd3322eebaaf450c3847e448eac1896765
Vlad Zusko <vladzvu11@gmail.com>
2024-03-15 00:44:39 +0200

    feat(connections/domain): add abort for core methods

--- src/packages/connections/domain/Communicator.ts
+++ src/packages/connections/domain/Communicator.ts
@@ -1,11 +1,13 @@
 import type { Group, Process } from '@/core/domain';
 
+import type Abort from './Abort';
+
 export default interface Communicator {
   isMaster: boolean;
   process: Process;
   group: Group;
   start(): Promise<void>;
-  send(data: unknown, processes: Process[]): Promise<void>;
-  receive(): Promise<unknown>;
-  broadcast(data: unknown): Promise<void>;
+  send(data: unknown, processes: Process[], abort?: Abort): Promise<void>;
+  receive(abort?: Abort): Promise<unknown>;
+  broadcast(data: unknown, abort?: Abort): Promise<void>;
 }
