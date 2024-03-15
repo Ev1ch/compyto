635ddfe6ca1da0d4d873a810e150c105d948a2e7
Vlad Zusko <vladzvu11@gmail.com>
2024-03-15 18:18:06 +0200

    build: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,14 @@
-import type { JestConfigWithTsJest } from 'ts-jest';
+import { pathsToModuleNameMapper, type JestConfigWithTsJest } from 'ts-jest';
 
 import tsconfig from './tsconfig.json';
+
+const { baseUrl } = tsconfig.compilerOptions;
 
 const CONFIG: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleDirectories: ['node_modules', tsconfig.compilerOptions.baseUrl],
+  modulePaths: [baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths),
 };
 
 export default CONFIG;
