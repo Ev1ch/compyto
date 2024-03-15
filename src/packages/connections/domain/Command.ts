d3ccffc9c2d4af81425dbcfaa19270dfbd507e34
Vlad Zusko <vladzvu11@gmail.com>
2024-03-15 22:07:47 +0200

    feat(connections/domain): add data type

--- src/packages/connections/domain/Command.ts
+++ src/packages/connections/domain/Command.ts
@@ -1,10 +1,12 @@
 import type { OperatorType } from '@/core/domain';
 
+import type Data from './Data';
+
 /**
  * This interface represents a command
  * to be executed on other peer in the network.
  */
 export default interface Command {
   operatorType: OperatorType;
-  data: unknown;
+  data: Data;
 }
--- src/packages/connections/domain/index.ts
+++ src/packages/connections/domain/index.ts
@@ -2,6 +2,7 @@ export type { default as Abort } from './Abort';
 export type { default as Command } from './Command';
 export type { default as Communicator } from './Communicator';
 export type { default as Connection } from './Connection';
+export type { default as Data, ProcessWithData } from './Data';
 export type { default as Device } from './Device';
 export { default as State } from './State';
 export type { default as URI } from './URI';
