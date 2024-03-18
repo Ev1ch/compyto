30dcc3a665abe0ad53a36f1fe404b6048ffa833a
Vlad Zusko <vladzvu11@gmail.com>
2024-03-19 00:16:06 +0200

    feat(logging/constants): make colors bold

--- src/packages/logging/constants.ts
+++ src/packages/logging/constants.ts
@@ -5,15 +5,15 @@ import { MonitoringEventScope, MonitoringEventType } from '@/monitoring/domain';
 import { Color, type Print } from './domain';
 
 export const COLOR_TO_PRINT_METHOD_MAP: Record<Color, Print> = {
-  [Color.RED]: chalk.red,
-  [Color.GREEN]: chalk.green,
-  [Color.YELLOW]: chalk.yellow,
+  [Color.RED]: chalk.red.bold,
+  [Color.GREEN]: chalk.green.bold,
+  [Color.YELLOW]: chalk.yellow.bold,
 
-  [Color.BLUE]: chalk.blue,
-  [Color.MAGENTA]: chalk.magenta,
-  [Color.CYAN]: chalk.cyan,
-  [Color.WHITE]: chalk.white,
-  [Color.BLACK]: chalk.black,
+  [Color.BLUE]: chalk.blue.bold,
+  [Color.MAGENTA]: chalk.magenta.bold,
+  [Color.CYAN]: chalk.cyan.bold,
+  [Color.WHITE]: chalk.white.bold,
+  [Color.BLACK]: chalk.black.bold,
 };
 
 export const EVENT_TYPE_TO_COLOR_MAP: Record<MonitoringEventType, Color> = {
