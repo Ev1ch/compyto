e83869ec6bedff49e40c10d7a584214a609e0954
Vlad Zusko <vladzvu11@gmail.com>
2024-03-19 00:19:04 +0200

    feat(runner/logic): add example event subscription and logging

--- src/packages/runner/logic/createRunner.ts
+++ src/packages/runner/logic/createRunner.ts
@@ -4,12 +4,18 @@ import { readFile } from 'node:fs/promises';
 import * as path from 'node:path';
 
 import { createSocketCommunicator } from '@/connections/sockets/logic';
+import logger from '@/logging/logic/logger';
+import { monitoring } from '@/monitoring/logic';
 
 import type { Runner } from '../domain';
 import { DEFAULT_SETTINGS_PATH } from '../constants';
 import { SettingsSchema } from '../schemas';
 
 export default async function createRunner(): Promise<Runner> {
+  monitoring.onAny(logger.event);
+
+  monitoring.emit('info:balancing/example', 1);
+
   const settingsPath = path.resolve(
     process.env.SETTINGS_PATH ?? DEFAULT_SETTINGS_PATH,
   );
@@ -25,3 +31,5 @@ export default async function createRunner(): Promise<Runner> {
     communicator,
   };
 }
+
+createRunner();
