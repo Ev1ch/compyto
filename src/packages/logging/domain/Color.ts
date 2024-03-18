5ea42a71ee884111b850964e0abb7b9bad6c319c
Vlad Zusko <vladzvu11@gmail.com>
2024-03-18 23:29:47 +0200

    feat(logging/domain): add color enum

---
+++ src/packages/logging/domain/Color.ts
@@ -0,0 +1,12 @@
+enum Color {
+  RED = 'red',
+  GREEN = 'green',
+  YELLOW = 'yellow',
+  BLUE = 'blue',
+  MAGENTA = 'magenta',
+  CYAN = 'cyan',
+  WHITE = 'white',
+  BLACK = 'black',
+}
+
+export default Color;
