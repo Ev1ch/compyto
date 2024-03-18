22912b5f890c100908293193488075a119733538
Vlad Zusko <vladzvu11@gmail.com>
2024-03-18 23:57:56 +0200

    feat(utils/logic): update type guard for events emitter creator

--- src/packages/utils/logic/createEventsEmitter.ts
+++ src/packages/utils/logic/createEventsEmitter.ts
@@ -1,9 +1,9 @@
-import EventEmitter from 'node:events';
+import NodeEventsEmitter from 'events';
 
 import type { EventsEmitter } from '../domain';
 
 export default function createEventsEmitter<
   TEvents extends Record<string, unknown[]>,
 >(): EventsEmitter<TEvents> {
-  return new EventEmitter<TEvents>();
+  return new NodeEventsEmitter();
 }
