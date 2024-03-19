8492f76658fc6f55132cab8c2770f2b77b019812
Vlad Zusko <vladzvu11@gmail.com>
2024-03-19 16:17:27 +0200

    docs(connections): add comment for abort and abort error

--- src/packages/connections/domain/Abort.ts
+++ src/packages/connections/domain/Abort.ts
@@ -1,3 +1,7 @@
+/**
+ * The interface which represents the
+ * structure of abort controller.
+ */
 export default interface Abort extends AbortController {
   abort(reason?: string): void;
 }
--- src/packages/connections/errors/AbortError.ts
+++ src/packages/connections/errors/AbortError.ts
@@ -1,5 +1,9 @@
 import { DEFAULT_ABORT_MESSAGE } from '../constants';
 
+/**
+ * Custom error class for aborting a connection
+ * and provide additional/default data.
+ */
 export default class AbortError extends Error {
   constructor(message = DEFAULT_ABORT_MESSAGE) {
     super(message);
