0531d526a57e440689e37d19809a44910573308c
Vlad Zusko <vladzvu11@gmail.com>
2024-03-19 15:19:22 +0200

    test: fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,14 @@ const { baseUrl } = tsconfig.compilerOptions;
 const CONFIG: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
   modulePaths: [baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths),
 };
