808d6fb27b653ad23553442355ca8b6155dc9f3c
Vlad Zusko <vladzvu11@gmail.com>
2024-03-27 21:48:39 +0200

    ci: fix packages building

---
+++ build.sh
@@ -0,0 +1,12 @@
+yarn workspace @compyto/validation build &&
+yarn workspace @compyto/core build &&
+yarn workspace @compyto/utils build &&
+yarn workspace @compyto/connections build &&
+yarn workspace @compyto/balancing build &&
+yarn workspace @compyto/monitoring build &&
+yarn workspace @compyto/settings build &&
+yarn workspace @compyto/sockets build &&
+yarn workspace @compyto/serialization build &&
+yarn workspace @compyto/logging build &&
+yarn workspace @compyto/runner build &&
+yarn workspace @compyto/dashboard build
