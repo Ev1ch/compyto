38980c3056c21c0a0dd3c3482bced3dbe12edcef
Vlad Zusko <vladzvu11@gmail.com>
2024-03-27 22:59:41 +0200

    ci: fix modules type

--- tests/jest.config.ts
+++ tests/jest.config.ts
@@ -3,11 +3,13 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const CONFIG: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
         tsconfig: 'tsconfig.json',
+        useESM: true,
       },
     ],
   },
