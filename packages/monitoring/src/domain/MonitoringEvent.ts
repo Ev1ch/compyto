d777d6d56c6519b005f20746a82146331bff5ba3
Vlad Zusko <vladzvu11@gmail.com>
2024-03-30 00:35:49 +0200

    feat(monitoring/domain): add args into event object

--- packages/monitoring/src/domain/MonitoringEvent.ts
+++ packages/monitoring/src/domain/MonitoringEvent.ts
@@ -3,9 +3,11 @@ import type MonitoringEventKey from './MonitoringEventKey';
 
 export default interface MonitoringEvent<
   TMonitoringEventKey extends MonitoringEventKey = MonitoringEventKey,
+  TMonitoringEventArgs extends unknown[] = unknown[],
   TMonitoringEventContext extends
     MonitoringEventContext = MonitoringEventContext,
 > {
   readonly key: TMonitoringEventKey;
+  readonly args: TMonitoringEventArgs;
   readonly context: TMonitoringEventContext;
 }
