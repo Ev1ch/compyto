6345346de5cbdf697e1f136fbb5f95c653e426ea
Vlad Zusko <vladzvu11@gmail.com>
2024-03-29 18:09:01 +0200

    feat(monitoring): add socket server starting

--- packages/monitoring/src/logic/createMonitoring.ts
+++ packages/monitoring/src/logic/createMonitoring.ts
@@ -1,17 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Server } from 'socket.io';
+
+import type { Settings } from '@compyto/settings';
 import { createEventsEmitter } from '@compyto/utils';
 
 import type { Monitoring, MonitoringEventKeysMap } from '../domain';
 import { ANY_MONITORING_EVENT_KEY } from '../constants';
 import getMonitoringEventContext from './getMonitoringEventContext';
 
-export default function createMonitoring(): Monitoring {
+export default function createMonitoring({
+  monitoring: {
+    uri: { port },
+  },
+}: Settings): Monitoring {
+  const io = new Server();
   const emitter = createEventsEmitter<MonitoringEventKeysMap>();
   const on = emitter.on.bind(emitter) as any;
   const off = emitter.off.bind(emitter) as any;
 
   const emit: Monitoring['emit'] = (eventKey, ...args) => {
     const eventContext = getMonitoringEventContext();
+    io.emit(eventKey, eventContext, ...args);
     // @ts-expect-error Argument of type...
     emitter.emit(eventKey, eventContext, ...(args as any));
     // @ts-expect-error Argument of type...
@@ -28,10 +37,9 @@ export default function createMonitoring(): Monitoring {
     emitter.off(ANY_MONITORING_EVENT_KEY, listener);
   };
 
+  io.listen(port);
+
   return {
-    context: {
-      process: null,
-    },
     on,
     off,
     onAny,
