6895c90a9b2d51198c8b0e01520b5af4c81a94b9
Vlad Zusko <vladzvu11@gmail.com>
2024-04-01 23:31:23 +0300

    feat(dashboard/monitoring): add domain entities re-exports

---
+++ packages/dashboard/src/modules/monitoring/domain/index.ts
@@ -0,0 +1,6 @@
+export type { default as MonitoringEventsFilter } from './MonitoringEventsFilter';
+export { default as MonitoringEventsFilterCriteria } from './MonitoringEventsFilterCriteria';
+export type { default as MonitoringEventsPreparers } from './MonitoringEventsPreparers';
+export type { default as MonitoringEventsSort } from './MonitoringEventsSort';
+export type { default as MonitoringEventsSortField } from './MonitoringEventsSortField';
+export { default as MonitoringEventsSortOrder } from './MonitoringEventsSortOrder';
