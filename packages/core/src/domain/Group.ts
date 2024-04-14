19b542c4df4e5e4959edb97d230f2b86a0ef3a0c
Paul-Vasylenko <maincrafter1307@gmail.com>
2024-04-14 16:16:19 +0300

    docs: add for communicator

--- packages/core/src/domain/Group.ts
+++ packages/core/src/domain/Group.ts
@@ -3,6 +3,7 @@ import type Process from './Process';
 /**
  * Interface which represents a group of processes
  * and provides methods to work with them.
+ * All communications will appear only inside of this group
  */
 export default interface Group {
   readonly processes: Process[];
