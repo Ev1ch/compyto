8a375acccc88f4e27a3c9a405decf910d17f4081
Vlad Zusko <vladzvu11@gmail.com>
2024-04-17 19:49:27 +0300

    feat: add dashboard creation in runtime

--- packages/dashboard/index.ts
+++ packages/dashboard/index.ts
@@ -1,15 +1,40 @@
 import path from 'path';
 import express from 'express';
+import open from 'open';
 
-import { URI } from '@compyto/connections';
+import { getStringURI } from '@compyto/connections';
+import { Settings } from '@compyto/settings';
 
 // @ts-expect-error Node modules versions differ
 const PUBLIC_PATH = path.resolve(import.meta.dirname, './client');
 
-export default function createDashboardServer({ port }: URI) {
+export interface Dashboard {
+  start(): Promise<void>;
+}
+
+export function createDashboard({
+  dashboard,
+  monitoring,
+}: Settings): Dashboard {
   const app = express();
 
   app.use(express.static(PUBLIC_PATH));
 
-  app.listen(port);
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(PUBLIC_PATH, 'index.html'));
+  });
+
+  function start() {
+    return new Promise<void>((resolve) => {
+      const uri = `${getStringURI(dashboard.uri)}/?uri=${getStringURI(monitoring.uri)}&code=${dashboard.code}`;
+
+      app.listen(dashboard.uri.port, () => {
+        open(uri).then(() => resolve(undefined));
+      });
+    });
+  }
+
+  return {
+    start,
+  };
 }
--- packages/runner/src/logic/createRunner.ts
+++ packages/runner/src/logic/createRunner.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import { readFile } from 'node:fs/promises';
 import * as path from 'node:path';
 
+import { createDashboard } from '@compyto/dashboard';
 import { createConsoleLogger } from '@compyto/logging';
 import { createMonitoring } from '@compyto/monitoring';
 import { runtime } from '@compyto/runtime';
@@ -22,12 +23,17 @@ export default async function createRunner(): Promise<Runner> {
   const monitoring = createMonitoring(settings);
   const communicator = createSocketCommunicator(settings);
   const logger = createConsoleLogger(communicator.process);
+  const dashboard = createDashboard(settings);
 
   runtime.settings = settings;
   runtime.monitoring = monitoring;
   runtime.logger = logger;
+  runtime.dashboard = dashboard;
 
   monitoring.onAny(logger.event);
+  await monitoring.start();
+  await dashboard.start();
+  await monitoring.waitForDashboard();
 
   return {
     settings,
