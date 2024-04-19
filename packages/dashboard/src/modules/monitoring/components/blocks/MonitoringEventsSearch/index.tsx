48d3801968efda02134447b067b0a6fb94bd1d32
Vlad Zusko <vladzvu11@gmail.com>
2024-04-19 23:12:22 +0300

    feat(dashboard/monitoring): add debounce for search

--- packages/dashboard/src/modules/monitoring/components/blocks/MonitoringEventsSearch/index.tsx
+++ packages/dashboard/src/modules/monitoring/components/blocks/MonitoringEventsSearch/index.tsx
@@ -1,11 +1,11 @@
-import { SxProps, TextField } from '@mui/material';
-import { ChangeEvent, memo } from 'react';
+import { debounce, TextField, type SxProps } from '@mui/material';
+import { ChangeEvent, memo, useCallback, useEffect, useState } from 'react';
 
 import { EMPTY_OBJECT } from '@/constants';
-import { useDispatch, useSelector } from '@/store/hooks';
+import { useDispatch } from '@/store/hooks';
 import { getArrayedSx } from '@/styles/logic';
 
-import { selectSearch, setSearch } from '../../../store';
+import { setSearch as setStoreSearch } from '../../../store';
 
 export interface MonitoringEventsSearchProps {
   readonly sx?: SxProps;
@@ -15,12 +15,23 @@ export default memo(function MonitoringEventsSearch({
   sx = EMPTY_OBJECT,
 }: MonitoringEventsSearchProps) {
   const dispatch = useDispatch();
-  const search = useSelector(selectSearch);
+  const [search, setSearch] = useState('');
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+  const debouncedSetStoreSearch = useCallback(
+    debounce((value: string) => {
+      dispatch(setStoreSearch(value));
+    }),
+    [dispatch],
+  );
+
+  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    dispatch(setSearch(value));
-  }
+    setSearch(value);
+  }, []);
+
+  useEffect(() => {
+    debouncedSetStoreSearch(search);
+  }, [search, debouncedSetStoreSearch]);
 
   return (
     <TextField
