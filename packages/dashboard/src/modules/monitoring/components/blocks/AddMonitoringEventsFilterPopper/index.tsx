fa74cf17d4ca8c949d6f0eecbf7f12eaf4b4f315
Vlad Zusko <vladzvu11@gmail.com>
2024-04-21 23:05:13 +0300

    Revert "feat(dashboard/monitoring): add outside click closing"
    This reverts commit e7e5d5cc4163335c573a2f7018a7bf601cb200ef.

--- packages/dashboard/src/modules/monitoring/components/blocks/AddMonitoringEventsFilterPopper/index.tsx
+++ packages/dashboard/src/modules/monitoring/components/blocks/AddMonitoringEventsFilterPopper/index.tsx
@@ -14,7 +14,7 @@ import {
   Typography,
   type SelectChangeEvent,
 } from '@mui/material';
-import { memo, useEffect, useRef, useState } from 'react';
+import { forwardRef, useState } from 'react';
 
 import { uniq } from '@compyto/utils';
 import {
@@ -22,7 +22,6 @@ import {
   type MonitoringEventsFilter,
 } from '@/modules/monitoring/domain';
 import { EMPTY_OBJECT } from '@/constants';
-import { useOutsideClick } from '@/hooks';
 import { useSelector } from '@/store/hooks';
 
 import { createMonitoringEventsFilter } from '../../../logic';
@@ -38,111 +37,107 @@ export interface AddMonitoringEventsFilterPopperProps {
   onClose?: () => void;
 }
 
-export default memo(function AddMonitoringEventsFilterPopper({
-  anchor,
-  onAdd,
-  onClose,
-  sx = EMPTY_OBJECT,
-}: AddMonitoringEventsFilterPopperProps) {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [criteria, setCriteria] = useState<MonitoringEventsFilterCriteria | ''>(
-    '',
-  );
-  const values = useSelector((state) =>
-    criteria ? selectValuesByCriteria(state, criteria) : [],
-  );
-  const availableCriterion = useSelector(selectAvailableCriterion);
-  const [value, setValue] = useState<string | ''>('');
-  const isOutsideClick = useOutsideClick(ref);
-  const availableValues = criteria ? uniq(values) : null;
-  const filter =
-    criteria && value ? createMonitoringEventsFilter(criteria, value) : null;
+export default forwardRef<HTMLDivElement, AddMonitoringEventsFilterPopperProps>(
+  function AddMonitoringEventsFilterPopper(
+    { anchor, onAdd, onClose, sx = EMPTY_OBJECT },
+    ref,
+  ) {
+    const [criteria, setCriteria] = useState<
+      MonitoringEventsFilterCriteria | ''
+    >('');
+    const values = useSelector((state) =>
+      criteria ? selectValuesByCriteria(state, criteria) : [],
+    );
+    const availableCriterion = useSelector(selectAvailableCriterion);
+    const [value, setValue] = useState<string | ''>('');
+    const availableValues = criteria ? uniq(values) : null;
+    const filter =
+      criteria && value ? createMonitoringEventsFilter(criteria, value) : null;
 
-  function handleCriteriaChange({
-    target,
-  }: SelectChangeEvent<MonitoringEventsFilterCriteria>) {
-    setCriteria(target.value as MonitoringEventsFilterCriteria);
-  }
-
-  function handleValueChange({ target }: SelectChangeEvent<string>) {
-    setValue(target.value);
-  }
-
-  function handleAdd() {
-    if (!filter) {
-      throw new Error('Filter is not defined');
+    function handleCriteriaChange({
+      target,
+    }: SelectChangeEvent<MonitoringEventsFilterCriteria>) {
+      setCriteria(target.value as MonitoringEventsFilterCriteria);
     }
 
-    onAdd?.(filter);
-  }
-
-  useEffect(() => {
-    if (isOutsideClick) {
-      onClose?.();
+    function handleValueChange({ target }: SelectChangeEvent<string>) {
+      setValue(target.value);
     }
-  }, [isOutsideClick, onClose]);
 
-  return (
-    <Popper
-      sx={sx}
-      placement="bottom-start"
-      anchorEl={anchor}
-      ref={ref}
-      disablePortal
-      open
-    >
-      <Paper
-        sx={{
-          width: 200,
-          p: 1.5,
-        }}
+    function handleAdd() {
+      if (!filter) {
+        throw new Error('Filter is not defined');
+      }
+
+      onAdd?.(filter);
+    }
+
+    return (
+      <Popper
+        sx={sx}
+        placement="bottom-start"
+        anchorEl={anchor}
+        ref={ref}
+        disablePortal
+        open
       >
-        <Stack spacing={1}>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Typography>Filter</Typography>
-            <IconButton onClick={onClose} sx={{ alignSelf: 'flex-end' }}>
-              <Close />
-            </IconButton>
-          </Box>
-
-          <FormControl size="small" fullWidth>
-            <InputLabel>Criteria</InputLabel>
-            <Select
-              size="small"
-              label="Criteria"
-              value={criteria}
-              onChange={handleCriteriaChange}
-              autoFocus
+        <Paper
+          sx={{
+            width: 200,
+            p: 1.5,
+          }}
+        >
+          <Stack spacing={1}>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
             >
-              {availableCriterion.map((criteria) => (
-                <MenuItem key={criteria} value={criteria}>
-                  {criteria}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          {availableValues && (
+              <Typography>Filter</Typography>
+              <IconButton onClick={onClose} sx={{ alignSelf: 'flex-end' }}>
+                <Close />
+              </IconButton>
+            </Box>
+
             <FormControl size="small" fullWidth>
-              <InputLabel>Value</InputLabel>
-              <Select label="Value" value={value} onChange={handleValueChange}>
-                {availableValues.map((value) => (
-                  <MenuItem key={value} value={value}>
-                    {value}
+              <InputLabel>Criteria</InputLabel>
+              <Select
+                size="small"
+                label="Criteria"
+                value={criteria}
+                onChange={handleCriteriaChange}
+                autoFocus
+              >
+                {availableCriterion.map((criteria) => (
+                  <MenuItem key={criteria} value={criteria}>
+                    {criteria}
                   </MenuItem>
                 ))}
               </Select>
             </FormControl>
-          )}
+            {availableValues && (
+              <FormControl size="small" fullWidth>
+                <InputLabel>Value</InputLabel>
+                <Select
+                  label="Value"
+                  value={value}
+                  onChange={handleValueChange}
+                >
+                  {availableValues.map((value) => (
+                    <MenuItem key={value} value={value}>
+                      {value}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
 
-          {filter && <Button onClick={handleAdd}>Add</Button>}
-        </Stack>
-      </Paper>
-    </Popper>
-  );
-});
+            {filter && <Button onClick={handleAdd}>Add</Button>}
+          </Stack>
+        </Paper>
+      </Popper>
+    );
+  },
+);
--- packages/dashboard/src/modules/monitoring/components/blocks/AddMonitoringEventsSortPopper/index.tsx
+++ packages/dashboard/src/modules/monitoring/components/blocks/AddMonitoringEventsSortPopper/index.tsx
@@ -14,11 +14,10 @@ import {
   Typography,
   type SxProps,
 } from '@mui/material';
-import { memo, useEffect, useRef, useState } from 'react';
+import { forwardRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { EMPTY_OBJECT } from '@/constants';
-import { useOutsideClick } from '@/hooks';
 import { selectAvailableSortFields } from '@/modules/monitoring/store';
 import type { Noop } from '@/utils';
 
@@ -37,105 +36,97 @@ export interface AddMonitoringEventsSortPopperProps {
   readonly onClose?: Noop;
 }
 
-export default memo(function AddMonitoringEventsSortPopper({
-  anchor,
-  onAdd,
-  onClose,
-  sx = EMPTY_OBJECT,
-}: AddMonitoringEventsSortPopperProps) {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [field, setField] = useState<MonitoringEventsSortField | ''>('');
-  const [order, setOrder] = useState<MonitoringEventsSortOrder | ''>('');
-  const availableFields = useSelector(selectAvailableSortFields);
-  const isOutsideClick = useOutsideClick(ref);
-  const filter =
-    field && order ? createMonitoringEventsSort(field, order) : null;
+export default forwardRef<HTMLDivElement, AddMonitoringEventsSortPopperProps>(
+  function AddMonitoringEventsSortPopper(
+    { anchor, onAdd, onClose, sx = EMPTY_OBJECT },
+    ref,
+  ) {
+    const [field, setField] = useState<MonitoringEventsSortField | ''>('');
+    const [order, setOrder] = useState<MonitoringEventsSortOrder | ''>('');
+    const availableFields = useSelector(selectAvailableSortFields);
+    const filter =
+      field && order ? createMonitoringEventsSort(field, order) : null;
 
-  function handleCriteriaChange({
-    target,
-  }: SelectChangeEvent<MonitoringEventsSortField>) {
-    setField(target.value as MonitoringEventsSortField);
-  }
-
-  function handleValueChange({
-    target,
-  }: SelectChangeEvent<MonitoringEventsSortOrder>) {
-    setOrder(target.value as MonitoringEventsSortOrder);
-  }
-
-  function handleAdd() {
-    if (!filter) {
-      throw new Error('Filter is not defined');
+    function handleCriteriaChange({
+      target,
+    }: SelectChangeEvent<MonitoringEventsSortField>) {
+      setField(target.value as MonitoringEventsSortField);
     }
 
-    onAdd?.(filter);
-  }
-
-  useEffect(() => {
-    if (isOutsideClick) {
-      onClose?.();
+    function handleValueChange({
+      target,
+    }: SelectChangeEvent<MonitoringEventsSortOrder>) {
+      setOrder(target.value as MonitoringEventsSortOrder);
     }
-  }, [isOutsideClick, onClose]);
 
-  return (
-    <Popper
-      sx={sx}
-      placement="bottom-start"
-      anchorEl={anchor}
-      ref={ref}
-      disablePortal
-      open
-    >
-      <Paper
-        sx={{
-          width: 200,
-          p: 1.5,
-        }}
+    function handleAdd() {
+      if (!filter) {
+        throw new Error('Filter is not defined');
+      }
+
+      onAdd?.(filter);
+    }
+
+    return (
+      <Popper
+        sx={sx}
+        placement="bottom-start"
+        anchorEl={anchor}
+        ref={ref}
+        disablePortal
+        open
       >
-        <Stack spacing={1}>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Typography>Sort</Typography>
-            <IconButton onClick={onClose} sx={{ alignSelf: 'flex-end' }}>
-              <Close />
-            </IconButton>
-          </Box>
-
-          <FormControl size="small" fullWidth>
-            <InputLabel>Criteria</InputLabel>
-            <Select
-              size="small"
-              label="Criteria"
-              value={field}
-              onChange={handleCriteriaChange}
-              autoFocus
+        <Paper
+          sx={{
+            width: 200,
+            p: 1.5,
+          }}
+        >
+          <Stack spacing={1}>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
             >
-              {availableFields.map((field) => (
-                <MenuItem key={field} value={field}>
-                  {field}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl size="small" fullWidth>
-            <InputLabel>Value</InputLabel>
-            <Select label="Value" value={order} onChange={handleValueChange}>
-              {MONITORING_EVENTS_SORT_ORDERS.map((order) => (
-                <MenuItem key={order} value={order}>
-                  {order}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+              <Typography>Sort</Typography>
+              <IconButton onClick={onClose} sx={{ alignSelf: 'flex-end' }}>
+                <Close />
+              </IconButton>
+            </Box>
 
-          {filter && <Button onClick={handleAdd}>Add</Button>}
-        </Stack>
-      </Paper>
-    </Popper>
-  );
-});
+            <FormControl size="small" fullWidth>
+              <InputLabel>Criteria</InputLabel>
+              <Select
+                size="small"
+                label="Criteria"
+                value={field}
+                onChange={handleCriteriaChange}
+                autoFocus
+              >
+                {availableFields.map((field) => (
+                  <MenuItem key={field} value={field}>
+                    {field}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl size="small" fullWidth>
+              <InputLabel>Value</InputLabel>
+              <Select label="Value" value={order} onChange={handleValueChange}>
+                {MONITORING_EVENTS_SORT_ORDERS.map((order) => (
+                  <MenuItem key={order} value={order}>
+                    {order}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            {filter && <Button onClick={handleAdd}>Add</Button>}
+          </Stack>
+        </Paper>
+      </Popper>
+    );
+  },
+);
