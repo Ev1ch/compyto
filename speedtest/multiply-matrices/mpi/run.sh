08ed8c76ad3e99febe63f182e5e8fd2380306da9
Paul-Vasylenko <maincrafter1307@gmail.com>
2024-04-22 22:09:19 +0300

    feat: finish speedtests

--- speedtest/multiply-matrices/mpi/run.sh
+++ speedtest/multiply-matrices/mpi/run.sh
@@ -8,7 +8,11 @@ gnome-terminal -- bash -c 'cd pc-1; node main.js; bash'
 gnome-terminal -- bash -c 'cd pc-2; node main.js; bash'
 
 # Open terminal for pc-2 process
-# gnome-terminal -- bash -c 'cd pc-3; node main.js; bash'
+gnome-terminal -- bash -c 'cd pc-3; node main.js; bash'
+gnome-terminal -- bash -c 'cd pc-4; node main.js; bash'
+gnome-terminal -- bash -c 'cd pc-5; node main.js; bash'
+gnome-terminal -- bash -c 'cd pc-6; node main.js; bash'
+gnome-terminal -- bash -c 'cd pc-7; node main.js; bash'
 
 # Open terminal for master process
 gnome-terminal -- bash -c 'cd master; node main.js; bash'
