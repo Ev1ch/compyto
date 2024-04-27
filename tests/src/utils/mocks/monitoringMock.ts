691be315b4b4e531c4946699d10dca05d3b43ff2
Vlad Zusko <vladzvu11@gmail.com>
2024-04-27 22:22:27 +0300

    test: fix monitoring mock

--- tests/src/utils/mocks/monitoringMock.ts
+++ tests/src/utils/mocks/monitoringMock.ts
@@ -3,11 +3,14 @@ import mock from 'jest-mock';
 import type { Monitoring } from '@compyto/monitoring';
 
 const monitoringMock: Monitoring = {
+  events: [],
   on: mock.fn(),
   onAny: mock.fn(),
   off: mock.fn(),
   offAny: mock.fn(),
   emit: mock.fn(),
+  start: mock.fn(() => Promise.resolve()),
+  waitForDashboard: mock.fn(() => Promise.resolve()),
 };
 
 export default monitoringMock;
