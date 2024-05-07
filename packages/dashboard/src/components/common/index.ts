8cb57ebbbbd97a6cc495c4a0f3f2e9511c8ef322
Vlad Zusko <vladzvu11@gmail.com>
2024-05-07 11:43:14 +0300

    feat(dashboard/components): extract baseline component

--- packages/dashboard/src/components/common/index.ts
+++ packages/dashboard/src/components/common/index.ts
@@ -1,6 +1,1 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-export { CssBaseline as Baseline } from '@mui/material';
+export { default as Baseline } from './Baseline';
