bd153648c4edf1e5f9931d03e51e0eba820fcd8c
Vlad Zusko <vladzvu11@gmail.com>
2024-05-26 02:21:43 +0300

    ci: fix version divider in script

--- publish.sh
+++ publish.sh
@@ -12,6 +12,6 @@ for package in ./packages/*; do
     echo "Publishing $PACKAGE_NAME@$PACKAGE_VERSION"
     yarn workspace $PACKAGE_NAME publish --access public
   else
-    echo "Skipping $PACKAGE_NAME@$PACKAGE_VERSION"
+    echo "Skipping $PACKAGE_NAME:$PACKAGE_VERSION"
   fi
 done
