187272e17ed9bdbe062930da4c786a2ccd9bd9a0
Vlad Zusko <vladzvu11@gmail.com>
2024-05-25 23:50:57 +0300

    ci: add publishing workflow

---
+++ publish.sh
@@ -0,0 +1,16 @@
+for package in ./packages/*; do
+  PACKAGE_VERSION="$(node -p -e "require('$package/package.json').version")"
+  PACKAGE_NAME="$(node -p -e "require('$package/package.json').name")"
+  FOUND_VERSION=$(npm view $PACKAGE_NAME versions | grep -w \'$PACKAGE_VERSION\')
+
+  if ! $FOUND_VERSION; then
+    IS_NEW_VERSION=true
+  fi
+
+  if $IS_NEW_VERSION; then
+    echo "Publishing $PACKAGE_NAME@$PACKAGE_VERSION"
+    yarn workspace $PACKAGE_NAME publish
+  else
+    echo "Skipping $PACKAGE_NAME@$PACKAGE_VERSION"
+  fi
+done
