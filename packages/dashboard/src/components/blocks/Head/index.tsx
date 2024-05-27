226a02339186d59f7791226f9736c682542c7401
Paul-Vasylenko <maincrafter1307@gmail.com>
2024-05-27 22:38:37 +0300

    fix: sonar issue

--- packages/dashboard/src/components/blocks/Head/index.tsx
+++ packages/dashboard/src/components/blocks/Head/index.tsx
@@ -8,6 +8,3 @@ export interface HeadProps {
 export default function Head({ children }: HeadProps) {
   return createPortal(children, document.head);
 }
-<>
-  <meta name="viewport" content="initial-scale=1, width=device-width" />
-</>;
